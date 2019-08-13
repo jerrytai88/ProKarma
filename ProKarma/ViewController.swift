ac10ce57401d4b8fa693d102390d8653520ca66b
Jerry <yutai@Yus-MacBook-Pro.local>
2019-08-13 09:29:03 -0400

    First Commit

---
+++ ProKarma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProKarma
+//
+//  Created by Yu Tai on 8/13/19.
+//  Copyright © 2019 Yu Tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
